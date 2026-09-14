3c91eb74c9dda064a485433e863aa2e0bbe26b99
Shlomi <shlomikaufman91@gmail.com>
2026-09-14 11:53:00 +0300

    feat: add global storage for favorites weathers

---
+++ frontend/src/store/useFavoritesStore.ts
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import type { Favorite, FavoriteBody } from "../types/favorite";
+import { persist } from "zustand/middleware";
+import api from "../api/api";
+
+interface FavoriteStoreProps {
+  favorites: Favorite[];
+  fetched: boolean;
+  fetchFavorites: (explorerName: string) => Promise<void>;
+  addFavorite: (favorite: FavoriteBody) => Promise<void>;
+  removeFavorite: (id: number) => Promise<void>;
+  clear: () => void;
+}
+
+export const useFavoritesStore = create<FavoriteStoreProps>()(
+  persist(
+    (set, get) => ({
+      favorites: [],
+      fetched: false,
+
+      fetchFavorites: async (explorerName) => {
+        if (get().fetched) return;
+        const res = await api.get<Favorite[]>("/favorites", {
+          params: { explorerName },
+        });
+        set({ favorites: res.data, fetched: true });
+      },
+
+      addFavorite: async (favorite) => {
+        const res = await api.post<Favorite>("/favorites", favorite);
+        set({ favorites: [...get().favorites, res.data] });
+      },
+
+      removeFavorite: async (id) => {
+        await api.delete(`/favorites/${id}`);
+        set({ favorites: get().favorites.filter((fav) => fav.id !== id) });
+      },
+
+      clear: () => set({ favorites: [], fetched: false }),
+    }),
+    {
+      name: "Favorites",
+      partialize: (state) => ({ favorites: state.favorites }),
+    },
+  ),
+);
