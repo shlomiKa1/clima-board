09841060f70a070337375381516a13c070de1673
Shlomi <shlomikaufman91@gmail.com>
2026-09-14 08:03:51 +0300

    chore: remove commented out sync attempt

--- frontend/src/routes/ProtectedRoutes.tsx
+++ frontend/src/routes/ProtectedRoutes.tsx
@@ -1,21 +1,7 @@
-import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoutes = () => {
   const explorerName = localStorage.getItem("explorerName");
-  //   const [explorerName, setExplorerName] = useState(() =>
-  //     localStorage.getItem("explorerName"),
-  //   );
-
-  //   useEffect(() => {
-  //     function handleStorageChange() {
-  //       setExplorerName(localStorage.getItem("explorerName"));
-  //     }
-
-  //     window.addEventListener("storage", handleStorageChange);
-
-  //     return () => window.removeEventListener("storage", handleStorageChange);
-  //   }, []);
 
   if (!explorerName) {
     return <Navigate to="/" replace />;
