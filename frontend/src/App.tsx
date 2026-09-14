f2ffdba10b63f714c8cd9726a1ec5ded9f783380
Shlomi <shlomikaufman91@gmail.com>
2026-09-14 08:05:16 +0300

    chore: remove unnecessary wrapper in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,27 +12,25 @@ import ProtectedPublic from "./routes/ProtectedPublic";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<ProtectedPublic />}>
-            <Route path="/" element={<WelcomePage />} />
-          </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<ProtectedPublic />}>
+          <Route path="/" element={<WelcomePage />} />
+        </Route>
 
-          <Route element={<ProtectedRoutes />}>
-            <Route element={<Layout />}>
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/search" element={<SearchPage />} />
-              <Route path="/city/:cityName" element={<CityDetailsPage />} />
-              <Route path="/favorites" element={<FavoritesPage />} />
-              <Route path="/compare" element={<ComparePage />} />
-            </Route>
+        <Route element={<ProtectedRoutes />}>
+          <Route element={<Layout />}>
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/city/:cityName" element={<CityDetailsPage />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+            <Route path="/compare" element={<ComparePage />} />
           </Route>
+        </Route>
 
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
